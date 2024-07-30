bc19f191a22c2311ed3d689b76eaa8068c22af7d
OttoReuter <detlef@dgebhardt.de>
2024-07-30 17:00:35 +0200

    Add files via upload

---
+++ Waveshare-LCD-Display-22668/Analogue-Digital-Clock/code.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText : 2024 Detlef Gebhardt, written for CircuitPython 8.2.4
+# SPDX-FileCopyrightText : Copyright (c) 2024 Detlef Gebhardt
+# SPDX-Filename          : pico-watch
+# SPDX-License-Identifier: https://dgebhardt.de
+import analog_digital
+
+analog_digital.run_analog_digital()
